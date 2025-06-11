7dbc7a5c1cadec8137c1319890339467fdf1629c
agsb <barcellos.alvaro@gmail.com>
2025-06-11 17:38:23 -0300

    update radioamador

--- radioamador/excludes.sh
+++ radioamador/excludes.sh
@@ -4,6 +4,7 @@ grep -v 'X' | \
 # do not confuse with prefix
 # grep -v '[87654321]P[PQRSTYU].' | \
 # do not allow repeats
-sed -e '/\([A-Z]\)\1./d; /\([A-Z]\).\1/d; /.\([A-Z]\)\1/d; ' | \
+#sed -e '/\([A-Z]\)\1./d; /\([A-Z]\).\1/d; /.\([A-Z]\)\1/d; ' | \
+sed -e '/\([A-Z]\)\1./d; /.\([A-Z]\)\1/d; ' | \
 # then list
 cat > $1.0s
